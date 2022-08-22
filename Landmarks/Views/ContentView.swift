a3112e8af80b74612406fce87c1954f00f0b025c
Dmytro Ivanchuk <dmytroivanchuk1998@gmail.com>
2022-08-22 18:55:25 +0300

    add missing files from last commit

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Dmytro Ivanchuk on 22.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
